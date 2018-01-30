7a149620fda4e66c1306396e5221904252f91d78
zellw <charlenezell@gmail.com>
2018-01-30 19:26:57 +0800

    extractsth

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 
 // in src/App.js
-import { PostList, PostEdit, PostCreate } from './posts';
-import { PostList2, PostEdit2, PostCreate2 } from './comments';
-import {TopicList,TopicListEdit,TopicListCreate} from './topic';
+import { PostList, PostEdit, PostCreate } from './sourceHandler/posts';
+import { PostList2, PostEdit2, PostCreate2 } from './sourceHandler/comments';
+import {TopicList,TopicListEdit,TopicListCreate} from './sourceHandler/topic';
 import restClientRouter from 'aor-rest-client-router';
-import customRestClient01 from "./customRestClient01";
+import customRestClient01 from "./restClients/customRestClient01";
 // import { UserList } from './users';
 
 const restRouter = restClientRouter({
